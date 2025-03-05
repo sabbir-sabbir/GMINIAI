5a64be2da3b7cfec4cc3ea8c2be444550a60818c
sabbir-sabbir <iosabbir7890@gmail.com>
2025-03-05 19:58:45 +0600

    add

--- gai/src/Components/Chatbot/Loading.jsx
+++ gai/src/Components/Chatbot/Loading.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Lottie from "lottie-react";
+import Lottie from "react-lottie-player";
 import loader from '../../assets/load.json'
 
 const Loading = () => {
   return (
     <>
     <div className="w-full h-screen flex-1 items-center justify-center">
-    <Lottie animationData={loader} className="w-80 h-80" />
+    <Lottie loop animationData={loader} play className="w-80 h-80" />
     </div>
     </>
   )
